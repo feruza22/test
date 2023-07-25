68b82a9b86063a11746da2f3549046769b16d068
feruza22 <feruza2218@gmail.com>
2023-07-25 01:08:36 -0700

    initiakl

---
+++ js/main.js
@@ -0,0 +1,82 @@
+ 
+//  var a = prompt ('enter number')
+//  var a = 'salom'
+// if (a%2==0){
+//     console.log('even number');
+//     }else if(a%2==1){
+//         console.log('odd number');
+//     }else{
+//       console.log('not number');
+//     }
+    //   vazifa
+// var a = prompt('enter number')
+
+// if(a%2==0){
+//     console.log('even number');
+// }else if(a%2==1){
+//     console.log('odd number');
+// }else{
+//     console.log('not number');
+// }
+    //  vazifa2
+//   var a =prompt('enter number')
+//   if (a>18){
+//     console.log('old number');
+//   }else if(a<18){
+//     console.log('little number');
+// } else{
+//     console.log('friends');
+// }
+        // //  vazifa3
+
+        //  var a =prompt('enter number')
+        // if(a>10 && a<20 ){
+        //     console.log('between');
+        // }else{
+        //     console.log('out');
+        // }
+
+
+        // vazifa bo'linuvchi
+var a =prompt('enter dividend')
+if(a%3==0 && a%7==0){
+    console.log('3  and 7 divident')
+}else if( a%3==0){
+    console.log('3divident');
+}else{ ( a%7==0)
+    console.log(' 7 divident');
+}
+        
+
+
+    // console.log(a**24);
+    // console.log(a**100);
+    // console.log(4/2);
+    // console.log(a%2==0);
+    // console.log(Math.floor(a));
+    // console.log(Math.round(a));
+    // console.log(Math.ceil (a));
+    // console.log(Math.floor(Math.random()*10)+10);
+    // console.log(a.toFixed(0)*10);
+   
+// if (5==6){
+//   console.log('true');  
+// }else{
+//     console.log(false);
+// }
+// console.log(5>5);
+// console.log(6>5);
+// console.log(6<5);
+// console.log(5<=5);
+// console.log(5>=5);
+// console.log(5!=5);
+// console.log(5!=6);
+// console.log(5<6 && 5==6);
+// console.log(5<6 || 5==6);
+// console.log(true<true-false|| true+true*3+4>9);
+// console.log(5=='5');
+
+
+
+
+
